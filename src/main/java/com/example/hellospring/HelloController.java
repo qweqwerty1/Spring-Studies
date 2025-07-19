1bec995dd0a7350d3197ddd3a4fa0528cb059876
JSP <suritongdal@naver.com>
2025-07-20 07:10:28 +0900

    feat: 동적 'Hello Spring' 페이지 구현 완료

--- src/main/java/com/example/hellospring/HelloController.java
+++ src/main/java/com/example/hellospring/HelloController.java
@@ -3,13 +3,14 @@ package com.example.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
-    @GetMapping("hello")
-    public String hello(Model model) {
-        model.addAttribute("data", "Spring!");
-        return "hello";
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
