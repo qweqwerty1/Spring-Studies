ea7bce1ca85a947c7d94186bb4acbed8c9ef573d
JSP <qweqwerty1@users.noreply.github.com>
2025-07-21 07:07:42 +0900

    feat : 회원가입 API 및 데이터베이스 연동 구현

---
+++ src/main/java/com/example/hellospring/controller/MemberController.java
@@ -0,0 +1,31 @@
+package com.example.hellospring.controller;
+
+import com.example.hellospring.domain.Member;
+import com.example.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping("/members/new")
+    @ResponseBody
+    public Member create(@RequestParam("name") String name) {
+        Member member = new Member();
+        member.setName(name);
+
+        memberService.join(member);
+
+        return member;
+    }
+}
---
+++ src/main/java/com/example/hellospring/service/MemberService.java
@@ -0,0 +1,27 @@
+package com.example.hellospring.service;
+
+import com.example.hellospring.domain.Member;
+import com.example.hellospring.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(member1 -> {throw  new IllegalStateException("이미 존재하는 회원입니다.");});
+    }
+}
